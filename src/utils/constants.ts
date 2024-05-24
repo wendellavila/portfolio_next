ccf8a0435a17fe83a6596d9479ec178f2357b0b7
wendellavila <wendelljcavila@gmail.com>
2024-05-24 11:26:13 -0300

    fix: Missing / in repoUrl

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -17,6 +17,6 @@ export const i18nConfig = {
 export const projectTitle = 'Title'; //'Wen\xADdell Jo\xADão Cas\xADtro de A\xADvi\xADla';
 export const deployUrl = 'https://wendellavila.github.io';
 export const githubUrl = 'https://github.com/wendellavila';
-export const repoUrl = `${githubUrl}portfolio_next`;
+export const repoUrl = `${githubUrl}/portfolio_next`;
 export const linkedinUrl = 'https://linkedin.com/in/wendellavila';
 export const email = 'wendelljcavila@gmail.com';
