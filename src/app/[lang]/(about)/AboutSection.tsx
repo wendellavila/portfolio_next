ca4a9570d920a56c85050b9f21505316cec09a43
wendellavila <wendelljcavila@gmail.com>
2024-05-27 10:36:27 -0300

    refactor: Using Tailwind media query instead of Hooks

--- src/app/[lang]/(about)/AboutSection.tsx
+++ src/app/[lang]/(about)/AboutSection.tsx
@@ -4,15 +4,11 @@ import { VerticalNav } from '@/components/VerticalNav';
 import { Avatar } from './Avatar';
 import { useTranslations } from 'next-intl';
 import { SectionTitle } from '@/components/SectionTitle';
-import { useScreenDimensions } from '@/utils/hooks';
 import { LinkList } from './LinkList';
 
 export function AboutSection(props: SectionProps) {
   const { componentRef, items } = props;
   const i18n = useTranslations('sections.about');
-
-  const [width] = useScreenDimensions();
-  const isMedium = width < 1024;
 
   return (
     <section
@@ -30,16 +26,14 @@ export function AboutSection(props: SectionProps) {
       <div className="grow flex flex-col pt-2 pb-6">
         <SectionTitle
           decorationClassName="bg-blueprint"
-          className={isMedium ? 'ml-6' : ''}
+          className="ml-6 md:ml-0"
         >
           {i18n('title')}
         </SectionTitle>
         <div
-          className={`flex gap-8 grow ${
-            isMedium
-              ? 'flex-col items-center'
-              : 'flex-row items-center justify-evenly'
-          }`}
+          className={`flex items-center gap-8 grow 
+          md:flex-row md:justify-evenly
+          flex-col`}
         >
           <div className="flex flex-col items-center px-4">
             <Avatar className="mb-4" src="/profile.png" size={200} alt="WA" />
